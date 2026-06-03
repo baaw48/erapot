c3f7f1c514ce072887d5d43a1a02c605880f9604
Baaw48 <ciputra.bahtiar@gmail.com>
2026-06-03 13:54:27 +0700

    fix: convert logo to base64 Data URI to completely avoid routing/URL path resolution issues

--- app/Models/Sekolah.php
+++ app/Models/Sekolah.php
@@ -24,9 +24,14 @@ class Sekolah extends Model
     public function getLogoUrl()
     {
         if ($this->logo_path) {
-            // Use public/logos/ directly to avoid symlink issues on Windows/cPanel
             $filename = basename($this->logo_path);
-            return asset('logos/' . $filename);
+            $fullPath = public_path('logos/' . $filename);
+            
+            if (file_exists($fullPath)) {
+                $mime = mime_content_type($fullPath);
+                $data = base64_encode(file_get_contents($fullPath));
+                return 'data:' . $mime . ';base64,' . $data;
+            }
         }
         return null;
     }
